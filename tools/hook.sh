e7c1b266cc6a838f12fc752e2df0c67961822b90
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-01-10 12:01:15 +0900

    refactor the script.

--- tools/hook.sh
+++ tools/hook.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
+SCRIPT_PATH=$(cd $(dirname $0) && pwd)/$(basename $0)
 
+WORKDIR=$(pwd)
 TIMESTAMP=$(date "+%Y/%m/%d-%H:%M:%S")
 CALLER=$(ps $PPID | tail -n 1 | awk '{c="";for(i=5;i<=NF;i++) c=c $i" "; print c}')
-echo -n "["
-echo -n "$TIMESTAMP"
-echo -n "]: "
-echo -n "("
-echo -n $CALLER
-echo -n ") "
-echo $*
+
+echo "[$TIMESTAMP] ($CALLER) work=$WORKDIR cmd=$SCRIPT_PATH $*"
